0d7d4d07b3165d70948148a9d52872a96019d912
MOGATON <mazurak7722@gmail.com>
2021-08-30 19:49:09 +0200

    Initial commit for TRON (Little Bank System)

---
+++ src/main/java/com/tron/TronApplication.java
@@ -0,0 +1,13 @@
+package com.tron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tron/TronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
